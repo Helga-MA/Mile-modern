288f75ef19ca519ef9a08a59b70e6254863938d1
Olga Stepanova <os20142020@gmail.com>
2021-09-01 01:24:58 +0200

    New commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,7 +2,6 @@ public class BonusMilesService {
     public int calculate(int price) {
         int mile = 20;
         int freeMiles = price / mile;
-        System.out.println("Free miles: " + freeMiles);
         return freeMiles;
     }
 
